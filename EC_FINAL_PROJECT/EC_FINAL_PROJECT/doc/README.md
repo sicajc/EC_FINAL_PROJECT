f8b441a5d3693f7fbfaff93a5d0156b9c0f5c171
User <kim4501@gmail.com>
2022-12-07 19:58:53 +0800

    please read my doc in verilog README

--- EC_FINAL_PROJECT/EC_FINAL_PROJECT/doc/README.md
+++
@@ -1,9 +0,0 @@
-# Hardware Specification
-## Top
-![TOP](https://user-images.githubusercontent.com/97605863/204489582-a0e5cab5-433e-42ad-8937-a69628f8f9f8.png)
-## Evaluate fitness
-![evaluate_fitness](https://user-images.githubusercontent.com/97605863/204489612-4de166cc-b8f7-4102-98f1-b1af7962c76e.png)
-## Generate Individual
-![generate_ind](https://user-images.githubusercontent.com/97605863/204489639-a21614ba-5bd7-4d74-b552-2622216cd5c8.png)
-## Population_rf
-![pop_rf](https://user-images.githubusercontent.com/97605863/204489664-2619f137-6410-4daf-96d3-d30d4c9e7ee9.png)
--- EC_FINAL_PROJECT/EC_FINAL_PROJECT/verilog/random_number_generater/LSFT_tb.v
+++ EC_FINAL_PROJECT/EC_FINAL_PROJECT/verilog/random_number_generater/LSFT_tb.v
@@ -1,63 +1,99 @@
 `timescale  1ns / 1ps
 `define CYCLE_TIME 1000
+`define CYCLE 10
 module tb_LSFR;
 
 // LSFR Parameters
-parameter PERIOD     = 10;
 parameter S_WIDTH    = 8;
-parameter INT_WIDTH  = 2;
+parameter IND_WIDTH  = 2;
+parameter RND_WIDTH  = 6;
 
-
-// LSFR Inputs
-reg   clk                                = 0 ;
-reg   rst_n                              = 0 ;
-reg   random_seed_valid                  = 0 ;
-reg   [1:0]  mode                        = 0 ;
-reg   [S_WIDTH-1:0]  random_seed         = 8'b00100110 ;
+reg clk=0;
+reg flag=0;
+reg [9:0] count=0;
+reg rst_n,in_valid;
+reg [S_WIDTH-1:0] random_seed_1;
+reg [S_WIDTH-1:0] random_seed_2;
+reg [S_WIDTH-1:0] random_seed_3;
 
 // LSFR Outputs
-wire[S_WIDTH-1:0]   mode_02_value ;
-wire[INT_WIDTH-1:0] mode_1_value  ;
+wire[S_WIDTH-1:0] random_num_ff_1 ;
+wire[S_WIDTH-1:0] random_num_ff_2 ;
+wire[S_WIDTH-1:0] random_num_ff_3 ;
 
-initial
+reg [IND_WIDTH-1:0] random_num_ff_2_o;
+reg [RND_WIDTH-1:0] random_num_ff_3_o;
+
+//clk
+always
 begin
-    forever #(PERIOD/2)  clk=~clk;
+  #(`CYCLE/2) clk = ~clk;
 end
 
 initial
 begin
-    #(PERIOD*2) rst_n  =  1;
-    #10;
-    rst_n   = 'd0;
-    random_seed_valid = 1'b1;
-    #50;
-    random_seed_valid = 1'b0;
-    mode    = 'd1;
-    #200;
-    mode    = 'd2;
-    #100;
-    mode    = 'd0;
-
-end
-
-LSFR #(
-    .S_WIDTH   ( S_WIDTH   ),
-    .INT_WIDTH ( INT_WIDTH ))
- u_LSFR (
-    .clk_i                                 ( clk                             ),
-    .rst_i                                 ( rst_n                           ),
-    .random_seed_valid_i                   ( random_seed_valid               ),
-    .mode_i                                ( mode                            ),
-    .random_seed_i                         ( random_seed                     ),
-
-    .random_num_ff_02_o  ( mode_02_value                ),
-    .random_num_ff_1_o   ( mode_1_value                 )
-);
-
-initial
-begin
+    rst_n = 0;
+    random_seed_1 = 'bx;
+    random_seed_2 = 'bx;
+    random_seed_3 = 'bx;
+    in_valid = 0;
+    #12 rst_n = 1;
+    @(negedge clk)
+    in_valid = 1;
+    random_seed_1 = 127;
+    random_seed_2 = 87;
+    random_seed_3 = 78;
+    @(negedge clk)
+    flag = 1;
+    in_valid = 0;
     #`CYCLE_TIME;
+    flag = 0;
     $finish;
 end
+
+always@(posedge clk)
+begin
+    if(flag) begin
+        count = count + 1;
+        random_num_ff_2_o = random_num_ff_2[IND_WIDTH-1:0];
+        random_num_ff_3_o = random_num_ff_3[RND_WIDTH-1:0];
+        $display("----------- display number %3d ------------",count);
+        $display("8 bit random_number : %3d,random_seed_1 is %3d",random_num_ff_1,random_seed_1);
+        $display("2 bit random_number : %3d,random_seed_2 is %3d",random_num_ff_2_o,random_seed_2);
+        $display("6 bit random_number : %3d,random_seed_3 is %3d",random_num_ff_3_o,random_seed_3);
+        $display("----------- ---------- ------------");
+    end
+end
+
+//module
+    LSFR #(
+            .S_WIDTH(S_WIDTH)
+        ) inst_LSFR_1 (
+            .clk               (clk),
+            .rst_n             (rst_n),
+            .random_seed_i     (random_seed_1),
+            .in_valid          (in_valid),
+            .random_num_ff_o   (random_num_ff_1)
+        );
+
+    LSFR #(
+            .S_WIDTH(S_WIDTH)
+        ) inst_LSFR_2 (
+            .clk               (clk),
+            .rst_n             (rst_n),
+            .random_seed_i     (random_seed_2),
+            .in_valid          (in_valid),
+            .random_num_ff_o   (random_num_ff_2)
+        );
+
+    LSFR #(
+            .S_WIDTH(S_WIDTH)
+        ) inst_LSFR_3 (
+            .clk               (clk),
+            .rst_n             (rst_n),
+            .random_seed_i     (random_seed_3),
+            .in_valid          (in_valid),
+            .random_num_ff_o   (random_num_ff_3)
+        );
 
 endmodule
