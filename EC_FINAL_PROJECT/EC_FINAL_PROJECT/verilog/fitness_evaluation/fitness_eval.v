17b614413090665fcf189760a40c2d24dbe0563d
Jacky Yeh <jackyyeh1999@gmail.com>
2022-12-09 13:41:51 +0800

    Code refactored

--- EC_FINAL_PROJECT/EC_FINAL_PROJECT/verilog/fitness_evaluation/fitness_eval.v
+++ EC_FINAL_PROJECT/EC_FINAL_PROJECT/verilog/fitness_evaluation/fitness_eval.v
@@ -8,6 +8,7 @@ module fitness_eval #(
            parameter INDIVIDUAL_LENGTH        = LATTICE_LENGTH * DATA_WIDTH,
            parameter POP_SIZE                 = 50
        ) (
+           //Inputs
            input clk_i,
            input rst_n,
            input [SELF_ENERGY_VEC_LENGTH   -1 : 0 ]     self_energy_vec_i,
@@ -15,32 +16,82 @@ module fitness_eval #(
            input [INDIVIDUAL_LENGTH   -1 :0]            individual_vec_i,
            input  in_valid_i,
            input  wr_initial_i,
+           input  ind_idx_i,
+
+           //Outputs
            output reg out_valid_ff_o,
            output reg done_ff_o,
-           output reg[SELF_FIT_LENGTH-1:0] total_energy_ff_o
+           output reg[SELF_FIT_LENGTH-1:0] total_energy_ff_o,
+           output reg ind_wb_idx_ff_o
        );
+//================================================================
+//  LOCAL PARAMETERS
+//================================================================
+//SelfEnergy = SE
+//IneractEnergy = IE
+localparam LV1_SE_ADDER_NUM = 5;
+localparam LV1_IE_ADDER_NUM = 5;
+
+localparam LV2_SE_ADDER_NUM = 3;
+localparam LV2_IE_ADDER_NUM = 2;
+
+localparam LV3_ADDER_NUM = 3;
+
+localparam CNT_WIDTH = 4;
+
 //================================================================
 //  INNER COMPONENTS
 //================================================================
+// Buffer stage
 reg[DATA_WIDTH - 1 :0] individual_buffer[0:LATTICE_LENGTH-1];
 reg in_valid_buf;
+reg ind_idx_buf;
 
-// RF
+// DF stage
 reg[DATA_WIDTH - 1 :0] self_energy_vec_rf[0:NUM_PARTICLE_TYPE-1];
 reg[DATA_WIDTH - 1 :0] interact_matrix_rf[0:NUM_PARTICLE_TYPE-1][0:NUM_PARTICLE_TYPE-1];
 
-// DF/ADD1
 reg[DATA_WIDTH - 1: 0] self_energy_DF_ADD1_pipe;
 reg[DATA_WIDTH - 1: 0] interact_energy_DF_ADD1_pipe;
 reg in_valid_DF_ADD1_pipe;
+reg ind_idx_DF_ADD1_pipe;
 
+// ADD1 stage
+wire[0:DATA_WIDTH]      self_energy_add_tree_lv1 [LV1_SE_ADDER_NUM - 1 :0];
+wire[0:DATA_WIDTH+1]    self_energy_add_tree_lv2 [LV2_SE_ADDER_NUM -1 :0];
+wire[0:DATA_WIDTH+2]    self_energy_add_tree_lv3 [LV3_ADDER_NUM -1 :0];
+
+wire[0:DATA_WIDTH]      interact_energy_add_tree_lv1[LV1_IE_ADDER_NUM - 1 :0];
+wire[0:DATA_WIDTH+1]    interact_energy_add_tree_lv2[LV2_IE_ADDER_NUM-1:0];
+
+wire[0:DATA_WIDTH+2]    partial_energy_add_tree_lv3[LV3_ADDER_NUM-1:0];
+
+reg[DATA_WIDTH+2 : 0]   partial_energy_ADD1_ADD2_pipe[0:LV3_ADDER_NUM-1];
+
+//ADD2 and output stage
+reg in_valid_ADD1_ADD2_pipe;
+reg ind_idx_add1_add2_pipe;
+
+reg[CNT_WIDTH -1:0] individual_cnt;
+
+wire[DATA_WIDTH*2+1 : 0] total_energy_wr;
+wire done_flag;
 
 //================================================================
-//  BUFFER STAGE
+//  GENERATE VARAIBLE
 //================================================================
+genvar adder_idx;
+
+//================================================================
+//  MAIN DESIGN
+//================================================================
+//===============================//
+//  BUFFER STAGE                 //
+//===============================//
 always @(posedge clk_i or negedge rst_n )
 begin : IN_VALID_BUF
     in_valid_buf <= ~rst_n ? 1'b1 : in_valid_i;
+    ind_idx_buf  <= ~rst_n ? 1'b1 : ind_idx_i;
 end
 
 
@@ -60,9 +111,9 @@ begin: IND_BUF
     end
 end
 
-//================================================================
-//  Data Fetch(DF) stage
-//================================================================
+//==============================//
+//  Data Fetch(DF) stage        //
+//==============================//
 always @(posedge clk_i or negedge rst_n)
 begin: SELF_ENERGY_VEC_RF
     integer i;
@@ -104,7 +155,10 @@ begin: INTERACT_MATRIX_RF
         end
 end
 
-// There might be problem here
+//=====================//
+//  DF/ADD1            //
+//=====================//
+
 always @(posedge clk_i or negedge rst_n)
 begin: SELF_ENERGY_DF_ADD1_PIPE
     integer i;
@@ -137,37 +191,14 @@ end
 
 always @(posedge clk_i or negedge rst_n )
 begin: IN_VALID_DF_ADD1_PIPE
-    in_valid_DF_ADD1_pipe <= ~rst_n ? 1'b1 : in_valid_buf;
+    in_valid_DF_ADD1_pipe      <= ~rst_n ? 1'b0 : in_valid_buf;
+    ind_idx_DF_ADD1_pipe       <= ~rst_n ? 1'b0 : ind_idx_buf;
 end
 
-//================================================================
-//  ADD1 stage
-//================================================================
-//SelfEnergy = SE
-//IneractEnergy = IE
-localparam LV1_SE_ADDER_NUM = 5;
-localparam LV1_IE_ADDER_NUM = 5;
-
-localparam LV2_SE_ADDER_NUM = 3;
-localparam LV2_IE_ADDER_NUM = 2;
-
-localparam LV3_ADDER_NUM = 3;
-
-wire[0:DATA_WIDTH]      self_energy_add_tree_lv1 [LV1_SE_ADDER_NUM - 1 :0];
-wire[0:DATA_WIDTH+1]    self_energy_add_tree_lv2 [LV2_SE_ADDER_NUM -1 :0];
-wire[0:DATA_WIDTH+2]    self_energy_add_tree_lv3 [LV3_ADDER_NUM -1 :0];
-
-wire[0:DATA_WIDTH]      interact_energy_add_tree_lv1[LV1_IE_ADDER_NUM - 1 :0];
-wire[0:DATA_WIDTH+1]    interact_energy_add_tree_lv2[LV2_IE_ADDER_NUM-1:0];
-
-wire[0:DATA_WIDTH+2]    partial_energy_add_tree_lv3[LV3_ADDER_NUM-1:0];
-
-reg[DATA_WIDTH+2 : 0] partial_energy_ADD1_ADD2_pipe[0:LV3_ADDER_NUM-1];
-
-reg in_valid_ADD1_ADD2_pipe;
-
+//=======================//
+//  ADD1 stage           //
+//=======================//
 //lv1.
-genvar adder_idx;
 generate
     for(adder_idx =0; adder_idx < LV1_SE_ADDER_NUM; adder_idx = adder_idx +1)
     begin: LV1_SE_adder_Tree1
@@ -205,6 +236,9 @@ assign partial_energy_add_tree_lv3[0] = self_energy_add_tree_lv2[0] + self_energ
 assign partial_energy_add_tree_lv3[1] = self_energy_add_tree_lv2[2] + interact_energy_add_tree_lv1[0];
 assign partial_energy_add_tree_lv3[2] = interact_energy_add_tree_lv2[0] + interact_energy_add_tree_lv2[1];
 
+//=====================//
+//  ADD1/ADD2          //
+//=====================//
 
 always @(posedge clk_i or negedge rst_n)
 begin: ADD1_ADD2_PIPE
@@ -222,17 +256,14 @@ end
 
 always @(posedge clk_i or negedge rst_n)
 begin: IN_VALID_ADD1_ADD2_PIPE
-    in_valid_ADD1_ADD2_pipe <= ~rst_n ? 'd0 : in_valid_DF_ADD1_pipe;
+    in_valid_ADD1_ADD2_pipe <= ~rst_n ? 1'd0 : in_valid_DF_ADD1_pipe;
+    ind_idx_add1_add2_pipe  <= ~rst_n ? 1'd0 : ind_idx_DF_ADD1_pipe;
 end
 
 
-//================================================================
-//  ADD2 stage
-//================================================================
-localparam CNT_WIDTH = 4;
-reg[CNT_WIDTH -1:0] individual_cnt;
-wire[DATA_WIDTH*2+1 : 0] total_energy_wr;
-wire done_flag;
+//=====================//
+//  ADD2 stage         //
+//=====================//
 
 //2 lv Adder tree
 assign total_energy_wr = (partial_energy_ADD1_ADD2_pipe[0] + partial_energy_ADD1_ADD2_pipe[1]) + partial_energy_ADD1_ADD2_pipe[2];
@@ -241,27 +272,27 @@ always @(posedge clk_i or negedge rst_n)
 begin: INDIVIDUAL_CNT
     if(~rst_n)
     begin
-      individual_cnt <= 'd0;
+        individual_cnt <= 'd0;
     end
     else if(done_ff_o)
     begin
-      individual_cnt <= 'd0;
+        individual_cnt <= 'd0;
     end
     else if(in_valid_ADD1_ADD2_pipe == 1'b1)
     begin
-      individual_cnt <= individual_cnt + 'd1;
+        individual_cnt <= individual_cnt + 'd1;
     end
     else
     begin
-      individual_cnt <= individual_cnt;
+        individual_cnt <= individual_cnt;
     end
 end
 
 assign done_flag = (individual_cnt == POP_SIZE-1);
 
-//================================================================
-//  OUTPUT stage
-//================================================================
+//====================//
+//  OUTPUT stage      //
+//====================//
 always @(posedge clk_i or negedge rst_n)
 begin: TOTAL_ENERGY_FF
     total_energy_ff_o <= ~rst_n ? 'd0 : total_energy_wr;
@@ -269,8 +300,9 @@ end
 
 always @(posedge clk_i or negedge rst_n)
 begin: OUTPUT_INDICATOR_SIGNAL
-    done_ff_o <= ~rst_n ? 1'b0 : done_flag;
-    out_valid_ff_o <= ~rst_n ? 1'b0 : in_valid_ADD1_ADD2_pipe;
+    done_ff_o           <= ~rst_n ? 1'b0 : done_flag;
+    out_valid_ff_o      <= ~rst_n ? 1'b0 : in_valid_ADD1_ADD2_pipe;
+    ind_wb_idx_ff_o     <= ~rst_n ? 1'b0 : ind_idx_add1_add2_pipe;
 end
 
 
